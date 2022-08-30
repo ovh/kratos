1177f82fba604b1e3424261f248df52c483d09eb
ThibaultHerard <thibaultherard10@gmail.com>
2022-08-30 15:12:58 +0000

    feat(saml): saml 2.0 implementation Signed-off-by: ThibaultHerard <thibaultherard10@gmail.com>
    Co-authored-by: sebferrer <sebferrer@users.noreply.github.com>
    Co-authored-by: psauvage <psauvage0@users.noreply.github.com>
    Co-authored-by: alexGNX <alexGNX@users.noreply.github.com>
    Co-authored-by: Stoakes <Stoakes@users.noreply.github.com>

---
+++ selfservice/strategy/saml/provider_saml.go
@@ -0,0 +1,67 @@
+package saml
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/crewjam/saml/samlsp"
+	"github.com/pkg/errors"
+
+	"github.com/ory/kratos/driver/config"
+	"github.com/ory/x/jsonx"
+)
+
+type ProviderSAML struct {
+	id     string
+	label  string
+	config *Configuration
+}
+
+func NewProviderSAML(
+	id string,
+	label string,
+	config *Configuration,
+) *ProviderSAML {
+	return &ProviderSAML{
+		id:     id,
+		label:  label,
+		config: config,
+	}
+}
+
+// Translate attributes from saml asseryion into kratos claims
+func (d *ProviderSAML) Claims(ctx context.Context, config *config.Config, attributeSAML samlsp.Attributes) (*Claims, error) {
+
+	var c ConfigurationCollection
+
+	conf := config.SelfServiceStrategy("saml").Config
+	if err := jsonx.
+		NewStrictDecoder(bytes.NewBuffer(conf)).
+		Decode(&c); err != nil {
+		return nil, errors.Wrapf(err, "Unable to decode config %v", string(conf))
+	}
+
+	providerSAML := c.SAMLProviders[len(c.SAMLProviders)-1]
+
+	claims := &Claims{
+		Issuer:        "saml",
+		Subject:       attributeSAML.Get(providerSAML.AttributesMap["id"]),
+		Name:          attributeSAML.Get(providerSAML.AttributesMap["firstname"]),
+		LastName:      attributeSAML.Get(providerSAML.AttributesMap["lastname"]),
+		Nickname:      attributeSAML.Get(providerSAML.AttributesMap["nickname"]),
+		Gender:        attributeSAML.Get(providerSAML.AttributesMap["gender"]),
+		Birthdate:     attributeSAML.Get(providerSAML.AttributesMap["birthdate"]),
+		Picture:       attributeSAML.Get(providerSAML.AttributesMap["picture"]),
+		Email:         attributeSAML.Get(providerSAML.AttributesMap["email"]),
+		Roles:         attributeSAML[providerSAML.AttributesMap["roles"]],
+		Groups:        attributeSAML[providerSAML.AttributesMap["groups"]],
+		PhoneNumber:   attributeSAML.Get(providerSAML.AttributesMap["phone_number"]),
+		EmailVerified: true,
+	}
+
+	return claims, nil
+}
+
+func (d *ProviderSAML) Config() *Configuration {
+	return d.config
+}
---
+++ selfservice/strategy/saml/strategy/strategy_login.go
@@ -0,0 +1,140 @@
+package strategy
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/ory/herodot"
+	"github.com/ory/kratos/continuity"
+	"github.com/ory/kratos/identity"
+	"github.com/ory/kratos/selfservice/flow"
+	"github.com/ory/kratos/selfservice/flow/login"
+	"github.com/ory/kratos/selfservice/flow/registration"
+	handler "github.com/ory/kratos/selfservice/flow/saml"
+	samlsp "github.com/ory/kratos/selfservice/strategy/saml"
+	"github.com/ory/kratos/session"
+	"github.com/ory/kratos/text"
+	"github.com/ory/kratos/ui/node"
+	"github.com/ory/kratos/x"
+)
+
+// Implement the interface
+var _ login.Strategy = new(Strategy)
+
+// Call at the creation of Kratos, when Kratos implement all authentication routes
+func (s *Strategy) RegisterLoginRoutes(r *x.RouterPublic) {
+	s.setRoutes(r)
+}
+
+// SubmitSelfServiceLoginFlowWithSAMLMethodBody is used to decode the login form payload
+// when using the saml method.
+//
+// swagger:model SubmitSelfServiceLoginFlowWithSAMLMethodBody
+type SubmitSelfServiceLoginFlowWithSAMLMethodBody struct {
+	// The provider to register with
+	//
+	// required: true
+	Provider string `json:"samlProvider"`
+
+	// The CSRF Token
+	CSRFToken string `json:"csrf_token"`
+
+	// Method to use
+	//
+	// This field must be set to `oidc` when using the oidc method.
+	//
+	// required: true
+	Method string `json:"method"`
+
+	// The identity traits. This is a placeholder for the registration flow.
+	Traits json.RawMessage `json:"traits"`
+}
+
+// Login and give a session to the user
+func (s *Strategy) processLogin(w http.ResponseWriter, r *http.Request, a *login.Flow, provider samlsp.Provider, c *identity.Credentials, i *identity.Identity, claims *samlsp.Claims) (*registration.Flow, error) {
+
+	var o CredentialsConfig
+	if err := json.NewDecoder(bytes.NewBuffer(c.Config)).Decode(&o); err != nil {
+		return nil, s.handleError(w, r, a, provider.Config().ID, nil, errors.WithStack(herodot.ErrInternalServerError.WithReason("The password credentials could not be decoded properly").WithDebug(err.Error())))
+	}
+
+	sess := session.NewInactiveSession()                                  //creation of an inactive session
+	sess.CompletedLoginFor(s.ID(), identity.AuthenticatorAssuranceLevel1) //Add saml to the Authentication Method References
+
+	if err := s.d.LoginHookExecutor().PostLoginHook(w, r, node.SAMLGroup, a, i, sess); err != nil {
+		return nil, s.handleError(w, r, a, provider.Config().ID, nil, err)
+	}
+
+	return nil, nil
+}
+
+func (s *Strategy) Login(w http.ResponseWriter, r *http.Request, f *login.Flow, ss *session.Session) (i *identity.Identity, err error) {
+	if err := login.CheckAAL(f, identity.AuthenticatorAssuranceLevel1); err != nil {
+		return nil, err
+	}
+
+	var p SubmitSelfServiceLoginFlowWithSAMLMethodBody
+	if err := s.newLinkDecoder(&p, r); err != nil {
+		return nil, s.handleError(w, r, f, "", nil, errors.WithStack(herodot.ErrBadRequest.WithDebug(err.Error()).WithReasonf("Unable to parse HTTP form request: %s", err.Error())))
+	}
+
+	var pid = p.Provider // this can come from both url query and post body
+	if pid == "" {
+		return nil, errors.WithStack(flow.ErrStrategyNotResponsible)
+	}
+
+	if err := flow.MethodEnabledAndAllowed(r.Context(), s.ID().String(), s.ID().String(), s.d); err != nil {
+		return nil, s.handleError(w, r, f, pid, nil, err)
+	}
+
+	req, err := s.validateFlow(r.Context(), r, f.ID)
+	if err != nil {
+		return nil, s.handleError(w, r, f, pid, nil, err)
+	}
+
+	if s.alreadyAuthenticated(w, r, req) {
+		return
+	}
+
+	state := x.NewUUID().String()
+	if err := s.d.RelayStateContinuityManager().Pause(r.Context(), w, r, sessionName,
+		continuity.WithPayload(&authCodeContainer{
+			State:  state,
+			FlowID: f.ID.String(),
+			Traits: p.Traits,
+		}),
+		continuity.WithLifespan(time.Minute*30)); err != nil {
+		return nil, s.handleError(w, r, f, pid, nil, err)
+	}
+
+	f.Active = s.ID()
+	if err = s.d.LoginFlowPersister().UpdateLoginFlow(r.Context(), f); err != nil {
+		return nil, s.handleError(w, r, f, pid, nil, errors.WithStack(herodot.ErrInternalServerError.WithReason("Could not update flow").WithDebug(err.Error())))
+	}
+
+	if x.IsJSONRequest(r) {
+		s.d.Writer().WriteError(w, r, flow.NewBrowserLocationChangeRequiredError(handler.RouteSamlLoginInit))
+	} else {
+
+		http.Redirect(w, r, handler.RouteSamlLoginInit, http.StatusSeeOther)
+	}
+
+	return nil, errors.WithStack(flow.ErrCompletedByStrategy)
+}
+
+func (s *Strategy) PopulateLoginMethod(r *http.Request, requestedAAL identity.AuthenticatorAssuranceLevel, l *login.Flow) error {
+	if l.Type != flow.TypeBrowser {
+		return nil
+	}
+
+	// This strategy can only solve AAL1
+	if requestedAAL > identity.AuthenticatorAssuranceLevel1 {
+		return nil
+	}
+
+	return s.populateMethod(r, l.UI, text.NewInfoLoginWith)
+}
