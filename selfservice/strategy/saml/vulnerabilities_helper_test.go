ad623e2483395d69a578755575590123540229da
sebferrer <sebastien@sebferrer.fr>
2022-12-22 17:32:57 +0000

    feat(kratos): test environment reorganization Signed-off-by: sebferrer <sebferrer@users.noreply.github.com>
    Co-authored-by: ThibaultHerard <thibaultherard10@gmail.com>

--- selfservice/strategy/saml/vulnerabilities_helper_test.go
+++ selfservice/strategy/saml/vulnerabilities_helper_test.go
@@ -1,9 +1,11 @@
 package saml_test
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/xml"
 	"net/http"
 	"net/http/httptest"
@@ -175,4 +177,63 @@ func NewTestIdpAuthnRequest(t *testing.T, idp *saml.IdentityProvider, acsURL str
 func MakeAssertion(t *testing.T, authnRequest *saml.IdpAuthnRequest, userSession *saml.Session) {
 	err := saml.DefaultAssertionMaker{}.MakeAssertion(authnRequest, userSession)
 	assert.NilError(t, err)
+}
+
+func prepareTestEnvironment(t *testing.T) (*MiddlewareTest, saml.IdpAuthnRequest, string) {
+	// Set timeNow for SAML Requests and Responses
+	setSAMLTimeNow("Wed Jan 1 01:57:09.123456789 UTC 2014")
+
+	// Create a SAML SP
+	testMiddleware, ts := NewMiddlewareTest(t)
+
+	// Create a SAML IdP
+	testIDP := NewIdentifyProviderTest(t, testMiddleware.Middleware.ServiceProvider, ts.URL)
+
+	// SP ACS URL
+	acsURL := ts.URL + "/self-service/methods/saml/acs/samlProvider"
+
+	// Create a SAML AuthnRequest as it would be taken into account by the IdP
+	// so that it can send the SAML Response back to the SP via the SP ACS
+	authnRequest, authnRequestID := NewTestIdpAuthnRequest(t, &testIDP.IDP, acsURL, testMiddleware.Middleware.ServiceProvider.EntityID)
+
+	return testMiddleware, authnRequest, authnRequestID
+}
+
+func PrepareTestSAMLResponse(t *testing.T) (*MiddlewareTest, saml.IdpAuthnRequest, string) {
+	testMiddleware, authnRequest, authnRequestID := prepareTestEnvironment(t)
+
+	// User session
+	userSession := &saml.Session{
+		ID:       "f00df00df00d",
+		UserName: "alice",
+	}
+
+	// Make SAML Assertion
+	MakeAssertion(t, &authnRequest, userSession)
+
+	// Make SAML Response
+	authnRequest.MakeResponse()
+
+	return testMiddleware, authnRequest, authnRequestID
+}
+
+func PrepareTestSAMLResponseHTTPRequest(t *testing.T, testMiddleware *MiddlewareTest, authnRequest saml.IdpAuthnRequest, authnRequestID string, responseStr string) *http.Request {
+	// Prepare SAMLResponse body attribute
+	v1 := &url.Values{}
+	v1.Set("SAMLResponse", base64.StdEncoding.EncodeToString([]byte(responseStr)))
+
+	// Set SAML AuthnRequest HTTP Request body with the SAML Response
+	req := authnRequest.HTTPRequest
+	req, err := http.NewRequest(req.Method, req.URL.String(), bytes.NewReader([]byte(v1.Encode())))
+	assert.NilError(t, err)
+
+	// Make tracked request and get its index
+	trackedRequestToken, trackedRequestIndex := testMiddleware.makeTrackedRequest(authnRequestID)
+
+	// Set SAML AuthnRequest HTTP Request headers Content-Type and session cookie
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", ""+
+		"saml_"+trackedRequestIndex+"="+trackedRequestToken)
+
+	return req
 }
--- selfservice/strategy/saml/vulnerabilities_test.go
+++ selfservice/strategy/saml/vulnerabilities_test.go
@@ -1,48 +1,18 @@
 package saml_test
 
 import (
-	"bytes"
-	"encoding/base64"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
 
 	"github.com/beevik/etree"
-	"github.com/crewjam/saml"
 
 	"gotest.tools/assert"
 	is "gotest.tools/assert/cmp"
 )
 
 func TestMiddlewareCanParseResponse(t *testing.T) {
-	// Set timeNow for SAML Requests and Responses
-	setSAMLTimeNow("Wed Jan 1 01:57:09.123456789 UTC 2014")
-
-	// Create a SAML SP
-	testMiddleware, ts := NewMiddlewareTest(t)
-
-	// Create a SAML IdP
-	testIDP := NewIdentifyProviderTest(t, testMiddleware.Middleware.ServiceProvider, ts.URL)
-
-	// SP ACS URL
-	acsURL := ts.URL + "/self-service/methods/saml/acs/samlProvider"
-
-	// Create a SAML AuthnRequest as it would be taken into account by the IdP
-	// so that it can send the SAML Response back to the SP via the SP ACS
-	authnRequest, authnRequestID := NewTestIdpAuthnRequest(t, &testIDP.IDP, acsURL, testMiddleware.Middleware.ServiceProvider.EntityID)
-
-	// User session
-	userSession := &saml.Session{
-		ID:       "f00df00df00d",
-		UserName: "alice",
-	}
-
-	// Make SAML Assertion
-	MakeAssertion(t, &authnRequest, userSession)
-
-	// Make SAML Response
-	authnRequest.MakeResponse()
+	testMiddleware, authnRequest, authnRequestID := PrepareTestSAMLResponse(t)
 
 	// Get Response Element
 	responseEl := authnRequest.ResponseEl
@@ -51,23 +21,9 @@ func TestMiddlewareCanParseResponse(t *testing.T) {
 
 	// Get Reponse string
 	responseStr, err := doc.WriteToString()
-
-	// Prepare SAMLResponse body attribute
-	v1 := &url.Values{}
-	v1.Set("SAMLResponse", base64.StdEncoding.EncodeToString([]byte(responseStr)))
-
-	// Set SAML AuthnRequest HTTP Request body with the SAML Response
-	req := authnRequest.HTTPRequest
-	req, err = http.NewRequest(req.Method, req.URL.String(), bytes.NewReader([]byte(v1.Encode())))
 	assert.NilError(t, err)
 
-	// Make tracked request and get its index
-	trackedRequestToken, trackedRequestIndex := testMiddleware.makeTrackedRequest(authnRequestID)
-
-	// Set SAML AuthnRequest HTTP Request headers Content-Type and session cookie
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookie", ""+
-		"saml_"+trackedRequestIndex+"="+trackedRequestToken)
+	req := PrepareTestSAMLResponseHTTPRequest(t, testMiddleware, authnRequest, authnRequestID, responseStr)
 
 	// Send the SAML Response to the SP ACS
 	resp := httptest.NewRecorder()
