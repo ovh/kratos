39508ad89533dbf45c25d509ce2d363c6b32767e
sebferrer <sebastien@sebferrer.fr>
2022-12-22 17:43:35 +0000

    feat(saml): vuln tests / add modified response test Signed-off-by: sebferrer <sebferrer@users.noreply.github.com>
    Co-authored-by: ThibaultHerard <thibaultherard10@gmail.com>

--- selfservice/strategy/saml/vulnerabilities_test.go
+++ selfservice/strategy/saml/vulnerabilities_test.go
@@ -32,3 +32,33 @@ func TestMiddlewareCanParseResponse(t *testing.T) {
 	// This is the Happy Path, the HTTP response code should be 302 (Found status)
 	assert.Check(t, is.Equal(http.StatusFound, resp.Code))
 }
+
+func TestMiddlewareParseModifiedResponse(t *testing.T) {
+	testMiddleware, authnRequest, authnRequestID := PrepareTestSAMLResponse(t)
+
+	// Get Response Element
+	responseEl := authnRequest.ResponseEl
+
+	// Add an attribute to the Response
+	responseEl.CreateAttr("newAttr", "randomValue")
+
+	doc := etree.NewDocument()
+	doc.SetRoot(responseEl)
+
+	// Get Reponse string
+	responseStr, err := doc.WriteToString()
+	assert.NilError(t, err)
+
+	req := PrepareTestSAMLResponseHTTPRequest(t, testMiddleware, authnRequest, authnRequestID, responseStr)
+
+	// Send the SAML Response to the SP ACS
+	resp := httptest.NewRecorder()
+	testMiddleware.Middleware.ServeHTTP(resp, req)
+
+	// This is the Happy Path, the HTTP response code should be 302 (Found status)
+	assert.Check(t, is.Equal(http.StatusForbidden, resp.Code))
+
+	/**
+	* TODO check signature is invalid error
+	 */
+}
